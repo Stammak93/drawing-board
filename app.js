e3c8c54dc2d1bb232a6889da746839bf07e133b6
Stammak93 <stamatis_makris93@yahoo.co.uk>
2022-01-09 16:00:27 +0000

    Added some extra checks for user image links and removed some redundant variables

--- app.js
+++ app.js
@@ -26,25 +26,71 @@ lineWidth.addEventListener("change", (e) => {
 })
 
 
-document.querySelector("#paste-btn").addEventListener("click", (e) => {
+document.querySelector("#clear-canvas").addEventListener("click", (e) => {
+    ctx.clearRect(0, 0, sketchboard.width, sketchboard.height)
+})
+
+
+const checkImageUrl = async (url) => {
+    const response = await fetch(url)
+    if (response.ok) {
+        return true
+    }
+}
+
+const checkImageFormat = (imageLink, formatList) => {
     
     let counter = 0
-    let validFormatList = ["jpg","svg","png","tiff","gif","bmp"]
-    let srcLink = document.querySelector("#paste-input").value
-
+    
     for(let i=0; i < 5; i++) {
-        if(srcLink.match(/\w+$/)[0] === validFormatList[i]) {
+        if(imageLink.match(/\w+$/)[0] === formatList[i]) {
             counter += 1
         }
     }
-    
+
     if (counter === 1) {
+        return 1
+    } else {
+        return 0
+    }
+}
+
+document.querySelector("#paste-btn").addEventListener("click", (e) => {
+    
+    let validFormatList = ["jpg","svg","png","tiff","gif","bmp"]
+    let srcLink = document.querySelector("#paste-input").value
+    let maxWidth;
+    let maxHeight;
+
+    let counter = checkImageFormat(srcLink,validFormatList)
+    let websiteLink = async () => await checkImageUrl(srcLink)
+
+    
+    // checking if link is valid image or if text contains valid image format
+    if (counter === 1 || websiteLink) {
+        
         let image = new Image()
         image.onload = function(){
-            ctx.drawImage(image, 0, 0, sketchboard.width, sketchboard.height)
+            if (image.width > 0) {
+                
+                // checking whether the image is larger than canvas
+                // and then drawing to canvas accordingly
+                if (image.width < sketchboard.width) {
+                    maxWidth = image.width
+                } else {
+                    maxWidth = sketchboard.width
+                }
+
+                if (image.height < sketchboard.height) {
+                    maxHeight = image.height
+                } else {
+                    maxHeight = sketchboard.height
+                }
+                console.log(maxHeight, maxWidth)
+                ctx.drawImage(image,Math.floor((sketchboard.width - image.width)/2),Math.floor((sketchboard.height - image.height)/2), maxWidth, maxHeight)
+            }
         }
         image.src = srcLink
-        document.querySelector("#paste-input").value = ""
     
     } else {
         alert("It appears that the link does not contain a valid image format.")    
@@ -57,8 +103,7 @@ document.querySelector("#paste-btn").addEventListener("click", (e) => {
 
 
 let ctx = sketchboard.getContext("2d")
-sketchboardOffsetLeft = sketchboard.offsetLeft
-sketchboardOffsetTop = sketchboard.offsetTop
+
 sketchboard.width = sketchboard.clientWidth
 sketchboard.height = sketchboard.clientHeight
 
@@ -80,13 +125,13 @@ const sketching = (e) => {
         isSketching = false
     }
 
-    ctx.lineTo(e.clientX - sketchboardOffsetLeft, e.clientY - sketchboardOffsetTop)
+    ctx.lineTo(e.clientX - sketchboard.offsetLeft, e.clientY - sketchboard.offsetTop)
     ctx.stroke()
 }
 
 sketchboard.addEventListener("mousedown", (e) =>{
-    sketchboardOffsetLeft = sketchboard.offsetLeft
-    sketchboardOffsetTop = sketchboard.offsetTop
+    sketchboard.offsetLeft
+    sketchboard.offsetTop
     isSketching = true
     startX = e.clientX;
     startY = e.clientY;
@@ -99,3 +144,27 @@ sketchboard.addEventListener("mouseup", (e) => {
 })
 
 sketchboard.addEventListener("mousemove", sketching)
+
+
+// ##################################################
+// Canvas image manipulation stuff
+
+//let image = document.querySelector("canvas.sketch")
+
+const imageManipulation = (data) => {
+    for (let i=0; i < data.length; i++) {
+        data[i] = 255
+    }
+}
+
+const imageEffect = () => {
+    imageData = ctx.getImageData(0, 0, sketchboard.width, sketchboard.height)
+
+    imageManipulation(imageData.data)
+
+    ctx.putImageData(imageData, 0, 0)
+}
+
+
+document.querySelector("#image-effect").addEventListener("click", imageEffect)
+
